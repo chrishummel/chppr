62358b053aa26c165817c96a1411d2a81728e0a5
Sam Fitz <abigwonderful@gmail.com>
2016-04-12 16:27:58 -0500

    more changes to server

--- server/server.js
+++ server/server.js
@@ -17,7 +17,6 @@ var flash    = require('connect-flash'); // messages stored in session
 var Posts = require('./models/posts');
 var Users = require('./models/users');
 
-var routes = express.Router()
 var app = express()
 
 
@@ -31,19 +30,15 @@ app.use(bodyParser.json())
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-// Mount our main router
-app.use('/', routes)
-
 // ---------- Routes Start Here ------------- //
 
 //Login route, default route
-routes.get('/', function(req, res) {
+app.get('/', function(req, res) {
 	res.sendFile(assetFolder + '/index.html')
 })
 
 //get endpoint for json obj for posts 
-routes.get('/feed', function (req, res) {
+app.get('/feed', function (req, res) {
 	Posts.loader()
 	.then(function(posts){
 		res.status(201).send(posts);
@@ -55,14 +50,14 @@ routes.get('/feed', function (req, res) {
 })
 
 //get endpoint to serve up index.html
-// routes.get('/dashboard', function (req, res) {
+// app.get('/dashboard', function (req, res) {
 // 	res.sendFile(assetFolder + '/index.html')
 // })
 
-routes.get('/pictures/')
+app.get('/pictures/')
 
 //post endpoint for user feed
-routes.post('/feed', function(req, res) {
+app.post('/feed', function(req, res) {
 	var card = req.body;
 	console.log("REQ BODY:", req.body);
 	Posts.create(card)
@@ -77,7 +72,7 @@ routes.post('/feed', function(req, res) {
 
 
 // endpoint thats only used to update categories table
-routes.post('/categories', function(req, res) {
+app.post('/categories', function(req, res) {
 	var cats = req.body;
 
 	Users.categories(cats)
@@ -91,39 +86,49 @@ routes.post('/categories', function(req, res) {
 })
 
 
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
 //Signup And login routes will be changed/deleted once auth is set up
-routes.post('/signup', function(req, res) {
-	var user = req.body;
+// app.post('/signup', function(req, res) {
+// 	var user = req.body;
 	
-	Users.create(user)
-	.then(function(person){
-		res.status(201).send(person);
-	})
-	.catch(function (err) {
-	console.log('Error creating new user: ', err);
-	return res.status(404).send(err);
-	})
-})
+// 	Users.create(user)
+// 	.then(function(person){
+// 		res.status(201).send(person);
+// 	})
+// 	.catch(function (err) {
+// 	console.log('Error creating new user: ', err);
+// 	return res.status(404).send(err);
+// 	})
+// })
 
 
-routes.post('/login', function (req, res) {
-	var user = req.body.username;
-	var pass = req.body.password;
+// app.get('/login', function (req, res) {
+// 	var user = req.body.username;
+// 	var pass = req.body.password;
 
-	Users.verify(user, pass).then(function (person) {
-		if (person){
-			res.status(201).send(person);
-		}
-		else {
-			res.status(400);
-			res.end('not a user')
-		}
-	})
-})
+// 	Users.verify(user, pass).then(function (person) {
+// 		if (person){
+// 			res.status(201).send(person);
+// 		}
+// 		else {
+// 			res.status(400);
+// 			res.end('not a user')
+// 		}
+// 	})
+// })
 
 /////// NOTE TO FUTURE GROUPS //////
 /////// THIS ALMOST KINDA WORKS ////
-// routes.post('/upload', function (req, res) {
+// app.post('/upload', function (req, res) {
 // 	var file = req.body;
 //   console.log("req body:", file);
 //   var path = "./client/pictures/test4.jpg"
@@ -141,11 +146,11 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // route for passport
-//require('./models/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+//require('./models/app.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // Static assets (html, etc.)
 var assetFolder = Path.resolve(__dirname, '../client')
-routes.use(express.static(assetFolder))
+app.use(express.static(assetFolder))
 
 var port = process.env.PORT || 4000
 app.listen(port)
